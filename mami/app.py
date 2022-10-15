77aa74e2576d4fa45b49aa895994c51e1525b018
mami-em <maminiainaravaloarison@gmail.com>
2022-10-15 11:43:47 -0400

    google search try success

--- mami/app.py
+++ mami/app.py
@@ -6,27 +6,31 @@ app = Flask('__name__')
 
 
 def get_weather(states):
-    url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={states}&units=metric&appid=b21a2633ddaac750a77524f91fe104e7')
 
+    url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={states}&units=metric&appid=b21a2633ddaac750a77524f91fe104e7')
     val = url.json()
 
     return val
 
 
-def re_formated(s):
+def re_formated(s, type):
     '''
-    Replace all spaces by '&'
+    Replace all spaces by '&' or by '+'
     '''
-    return s.replace(' ', '&')
+    if type == 'api':
+        return s.replace(' ', '&')
+
+    return s.replace(' ', '+')
 
 
 @app.route('/')
 def index():
 
     states = 'new york'
+    h = get_weather(re_formated(states, 'api'))
 
-    h = get_weather(re_formated(states))
+    google_search = f"https://www.google.com/search?q={re_formated(h['weather'][0]['description'], 'google')}"
 
-    return h['weather'][0]['description']
+    return redirect(google_search)
 
 
