5dc8104de4df34a454f7215883178042893d9767
mami-em <maminiainaravaloarison@gmail.com>
2022-10-15 11:14:10 -0400

    add mami source

---
+++ mami/app.py
@@ -0,0 +1,32 @@
+import sys
+import requests
+from flask import Flask, request, render_template, redirect
+
+app = Flask('__name__')
+
+
+def get_weather(states):
+    url = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={states}&units=metric&appid=b21a2633ddaac750a77524f91fe104e7')
+
+    val = url.json()
+
+    return val
+
+
+def re_formated(s):
+    '''
+    Replace all spaces by '&'
+    '''
+    return s.replace(' ', '&')
+
+
+@app.route('/')
+def index():
+
+    states = 'new york'
+
+    h = get_weather(re_formated(states))
+
+    return h['weather'][0]['description']
+
+
