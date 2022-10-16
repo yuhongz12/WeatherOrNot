c6fd403153896d338252834f5f35cc739bb6e31c
mami-em <maminiainaravaloarison@gmail.com>
2022-10-16 04:44:44 -0400

    commit changes

--- mami/app.py
+++ mami/app.py
@@ -1,8 +1,6 @@
 from crypt import methods
-import sys
-import json
-import requests, random
-from flask import Flask, request, render_template, redirect, jsonify
+import sys, json, csv, requests, random
+from flask import Flask, request, render_template, jsonify
 import country
 
 app = Flask('__name__')
@@ -28,6 +26,45 @@ def re_formated(s):
     return s.replace(' ', '+')
 
 
+def get_message(type):
+    '''
+    get random message depending on the type of weather we have
+    '''
+    a = list()
+    b = list()
+    with open(f'./pretentious/{type}.csv') as file:
+        for item in file:
+            a.append(item.strip())
+
+
+    with open(f'./slang/{type}.csv') as file:
+        for item in file:
+            b.append(item.strip())
+
+    return {'pretentious': a[random.randint(0, len(a) - 1)], 'slang':b[random.randint(0, len(b) - 1)]}
+
+
+def compare_message(country, case):
+
+    hot_messages = [f"It is so much warmer in {country}, why don't you Go there?", f"The weather so nice in {country}!", f"If the word perfect was a place it would be {country}", f"Weather in {country} is actually better."]
+    cold_messages = [f"It is much more fresher in {country}, Book a flight now!", f"I'd rather be in {country} if I were You.", f"You wishe you were in {country}, don't you?", f"You could have been in {country} tho, having nice time"]
+    
+    if case == 'hot':
+        return hot_messages[random.randint(0, len(hot_messages) - 1)] 
+
+    return cold_messages[random.randint(0, len(cold_messages) - 1)]
+
+
+def w_type(temp):
+    if 23 < temp:
+        return 'hot'
+
+    elif 18 <= temp <= 23:
+        return 'nice'
+
+    elif temp < 18:
+        return 'cold'
+
 
 @app.route('/')
 def index():
@@ -46,12 +83,6 @@ def locate():
 
     other_countries = []
 
-    # link
-    # https://en.wikipedia.org/wiki/{country_name}
-
-    # flag
-    # https://countryflagsapi.com/png/{country_name}
-
     # give only
     for i in range(5):
         w = get_weather(re_formated(country.all_country[random.randint(0,194)]['country']))
@@ -59,19 +90,17 @@ def locate():
         other_countries.append(w)
 
 
-    # FECTCH
-    # data['name']
-    # data['main']['temp']
-
     to_render = list()
 
 
     for city in other_countries:
 
+        type = w_type(city['main']['temp'])
+
         if city['main']['temp'] > data['main']['temp']:
             weather = {
                 'country_name': city['name'],
-                'message': 'It is much more warm',
+                'message': compare_message(city['name'], 'hot'),
                 'flag_img': f"https://countryflagsapi.com/png/{re_formated(city['name'])}",
                 'details_link': f"https://en.wikipedia.org/wiki/{re_formated(city['name'])}"
             }
@@ -80,7 +109,7 @@ def locate():
         else:
             weather = {
                 'country_name': city['name'],
-                'message': 'It is much more fresher',
+                'message': compare_message(city['name'], 'cold'),
                 'flag_img': f"https://countryflagsapi.com/png/{re_formated(city['name'])}",
                 'details_link': f"https://en.wikipedia.org/wiki/{re_formated(city['name'])}"
             }
@@ -91,7 +120,7 @@ def locate():
 
     data_details = {
         'country_name': data['name'],
-        'message': 'Our Funny message',
+        'message': get_message(type),
         'weather': data['weather'][0]['description']
     }
 
@@ -103,5 +132,3 @@ def locate():
     final_render.append(all_details)
 
     return jsonify(final_render)
-
-
